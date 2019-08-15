0c8484a706cd9949f7499a690885254e9f4c4e04
Guilherme Solon da Costa <guilherme@beg.inf.br>
2019-08-15 15:18:52 -0300

    Acerto da contagem por minutos.

--- beg_fnc_valida_tempo_rper_l.sql
+++ beg_fnc_valida_tempo_rper_l.sql
@@ -10,9 +10,9 @@ DECLARE
 V_RETORNO integer;
 V_DADO_TABELA integer;
 BEGIN
-select DATE_PART('hour', current_timestamp - min(rper_data_cadastro)) INTO V_DADO_TABELA from rper_recebimento_periferico_l;
+select (DATE_PART('Hour', current_timestamp - min(rper_data_cadastro))*60) + DATE_PART('minutes', current_timestamp - min(rper_data_cadastro)) INTO V_DADO_TABELA from rper_recebimento_periferico_l;
 
-IF V_DADO_TABELA >= p_horas THEN
+IF V_DADO_TABELA >= (p_horas*60) THEN
    RETURN 1;
 ELSE
 RETURN 0;
