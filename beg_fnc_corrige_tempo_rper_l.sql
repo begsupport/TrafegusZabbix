a43e82937ef4b09cc34f696ee8516e4795ec00b9
guilhermesolon <guilherme@beg.inf.br>
2019-08-16 11:14:56 -0300

    Funcao para execucao

--- beg_fnc_corrige_tempo_rper_l.sql
+++ beg_fnc_corrige_tempo_rper_l.sql
@@ -13,8 +13,10 @@ V_SQL varchar(300);
 BEGIN
 v_sql:='set statement_timeout to 0';
 execute v_sql;
-v_sql:='delete from rper_recebimento_periferico_l WHERE RPER_Data_Cadastro < current_timestamp - INTERVAL '||''''||(p_horas*60+1)||' Minutes'||'''';
+v_sql:='delete from rper_recebimento_periferico_l WHERE RPER_Data_Cadastro < current_timestamp - INTERVAL '||''''||p_horas||' Hours'||'''';
 execute v_sql;
 RETURN 0;
 END;
 $BODY$;
+
+select beg_fnc_corrige_tempo_rper_l();
